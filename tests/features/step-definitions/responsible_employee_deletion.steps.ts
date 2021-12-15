5ffb3ed1e4f8a6ae384237ab3db3840f38c4baba
Ivan <ilitteri@fi.uba.ar>
2021-12-15 12:06:45 -0300

    Add: task employee addition & deletion feature

---
+++ tests/features/step-definitions/responsible_employee_deletion.steps.ts
@@ -0,0 +1,33 @@
+import { Given, When, Then } from "cucumber";
+
+
+Given("I am on the task modification form", function() {
+	// Write code here that turns the phrase above into concrete actions
+	return "pending";
+});
+
+Given("I delete a responsible employee from the task that I am editing", function() {
+	// Write code here that turns the phrase above into concrete actions
+	return "pending";
+});
+
+When("I save the changes", function() {
+	// Write code here that turns the phrase above into concrete actions
+	return "pending";
+});
+
+Then("the employee will be deleted from the task.", function() {
+	// Write code here that turns the phrase above into concrete actions
+	return "pending";
+});
+
+When("I cancel the changes", function() {
+	// Write code here that turns the phrase above into concrete actions
+	return "pending";
+});
+
+Then("the employee will not be deleted from the task.", function() {
+	// Write code here that turns the phrase above into concrete actions
+	return "pending";
+});
+
